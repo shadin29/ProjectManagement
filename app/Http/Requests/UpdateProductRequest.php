6c007c5462622bae1b0bc619c5f8245a59cc82c0
shadin29 <150572232+shadin29@users.noreply.github.com>
2024-03-04 00:52:16 +0600

    Crud

---
+++ app/Http/Requests/UpdateProductRequest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateProductRequest extends FormRequest
+{
+   
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+
+    public function rules(): array
+    {
+        return [
+            'code' => 'required|string|max:50|unique:products,code,'.$this->product->id,
+            'name' => 'required|string|max:250',
+            'quantity' => 'required|integer|min:1|max:10000',
+            'price' => 'required',
+            'description' => 'nullable|string'
+        ];
+    }
+}
